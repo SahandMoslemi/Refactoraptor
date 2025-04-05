2830962f0ca6c9e011f0c92cd5fc1a88cc87b522
Muammer Buğra Kurnaz <muammerkurnaz1071@gmail.com>
2025-04-05 10:30:48 +0300

    add prompt engineering service

--- backend/src/main/java/org/refactoraptor/backend/Controller.java
+++ backend/src/main/java/org/refactoraptor/backend/Controller.java
@@ -10,13 +10,22 @@ import java.util.Map;
 public class Controller {
 
     private final OllamaService ollamaService;
+    private final IPromptEngineeringService promptEngineeringService;
 
-    public Controller(OllamaService ollamaService) {
+    public Controller(OllamaService ollamaService, IPromptEngineeringService promptEngineeringService) {
         this.ollamaService = ollamaService;
+        this.promptEngineeringService = promptEngineeringService;
     }
 
     @GetMapping("/models")
     public Map<String, Object> getModels() {
         return ollamaService.getModels();
     }
+
+    @GetMapping("/strategies")
+    public Map<String, Object> getStrategies() {
+        return promptEngineeringService.getStrategies();
+    }
+
+    
 }
